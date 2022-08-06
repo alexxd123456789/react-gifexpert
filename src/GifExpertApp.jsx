ed9e90a4ce224fbae60b82dc5b2b84eee6314097
Yasai <alexvidalcasado@gmail.com>
2022-08-06 11:21:38 +0200

    Primer Commit

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+import { AddCategory, GifGrid } from './components'
+
+
+
+export const GifExpertApp = () => {
+    
+    const [categories, setCategories] = useState(['One Punch'])
+    
+    const onAddCategory = (cat) => {
+
+        if(categories.includes(cat)) {
+            console.warn('Ya existe esa categoria');
+            return;
+        }
+        
+        
+        setCategories([cat, ...categories ]);
+    }
+
+
+
+    return (
+        <>
+
+            <h1>GifExpertApp</h1>
+
+            <AddCategory 
+            onAddCategory={onAddCategory} 
+            />
+    
+            {
+            categories.map( category => (
+                <GifGrid 
+                category={category} 
+                key={category} 
+                />
+                )
+                
+            )}
+
+        </>
+    )
+}
---
+++ src/components/AddCategory.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+export const AddCategory = ({onAddCategory}) => {
+
+    const [inputValue, setInputValue] = useState('');
+
+    const onInputChange = event => {
+        setInputValue(event.target.value);
+    }
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+
+        const newCat = inputValue.trim();
+
+
+        if (newCat.length < 1) return;
+
+        onAddCategory(newCat);
+        setInputValue("");
+    }
+
+
+  return (
+    <form onSubmit={onSubmit}>
+        <input 
+        type="text" 
+        placeholder="Buscar GIFs"
+        value={inputValue}
+        onChange={onInputChange}
+        />
+    </form>
+    
+    )
+}
